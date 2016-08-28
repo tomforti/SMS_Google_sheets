b2d24ae2b6406c6ef864b1e7a2184a807728f95f
tomforti <tomforti@users.noreply.github.com>
2016-08-27 22:41:18 -0400

    Added Fetures
    I added a Clear function as well as time stamping in order to log when messages were sent.

--- Script.java
+++ Script.java
@@ -5,8 +5,8 @@ function sendSms(to, body) {
 
   var messages_url = "https://api.twilio.com/2010-04-01/Accounts/" + SID + "/Messages.json"; 
   var payload = {
-    "To": to, //This will be the Cell Number from Cell
-    "Body" : body, //This will be your message from Cell 
+    "To": to, 
+    "Body" : body,  
     "From" : "Twilio Number"
   };
 
@@ -44,39 +44,46 @@ function lookup(phoneNumber) {
 
 
 //Main part of the code. This is where you will make changes to match your sheet. 
-function sendAll() {
-  var width = 7  //*************How wide is your cell (A-B is 2, A-G is 7 and so on)******************
-  var startRow = 3; //**********The row to start on change the 2 if numbers start lower down**********
-  var numbers = 3  //*************What Column are cell phone numbers in(A=0, B=1 and so on)******************
-  var msgcell = 6 //*************What Column is your messages written in(A=0, B=1 and so on)******************
-  var stscell = 6 //*************What Column is your status of the app printed in(A=1, B=2 and so on)******************
+unction sendAll() {
+  var width = 11  //*************How wide is your cell (A-B is 2, A-G is 7 and so on)******************
+  var startRow = 4; //**********The row to start on change the 2 if numbers start lower down**********
+  var numbers = 4  //*************What Column are cell phone numbers in(A=0, B=1 and so on)******************
+  var msgcell = 10 //*************What Column is your messages written in(A=0, B=1 and so on)******************
+  var stscell = 10 //*************What Column is your status of the app printed in(A=1, B=2 and so on)******************
+  var timecell = 8 //*************What Column is your timestamp of the app printed in(A=1, B=2 and so on)*****************
+  var leftmsgcell = 6 //*************What Column is your Left Message of the app printed in(A=1, B=2 and so on)*****************
 
 
   var sheet = SpreadsheetApp.getActiveSheet();//script runs on the sheet you have opened
   var numRows = sheet.getLastRow() - 1; //Figures out the last row
   var dataRange = sheet.getRange(startRow, 1, numRows, width) 
   var cellData = dataRange.getValues();
+  var datestamp = Utilities.formatDate(new Date(), "GMT-4:00", "EEE, MMM d, yyyy");  //*************Change your time zone here************
+  sheet.getRange(2, 1).setValue("DATE: " + datestamp); //************Loacton on where to place the Date(row,column), A=1******************
+
 
 for (var i in cellData) { 
         var spreadsheetRow = startRow + Number(i);
         var info = cellData[i];
-        var phoneNumber =  info[numbers]
+        var phoneNumber =  info[numbers] 
+        var timestamp = Utilities.formatDate(new Date(), "GMT-4:00", "h:mm a"); //*************Change your time zone here************
         sheet.getRange(spreadsheetRow, stscell).setValue("");
         if (phoneNumber != "") { 
             try { 
                 data = lookup(phoneNumber);
                 if (data['status'] == 404) { 
-                    sheet.getRange(spreadsheetRow, stscell).setValue("not found"); //This tells the script where to write message status to change 
+                    sheet.getRange(spreadsheetRow, stscell).setValue("not found");  
                 } 
                 else if (data['carrier']['type'] == "mobile") {
                     try {
-                        response_data = sendSms(info[numbers], info[msgcell]);  // Row[A] is "To" (the [] is the column number), Row[B] is "Body" (the [] is the column number) (0=A) CHANGE
-                        status = "sent";
+                        response_data = sendSms(info[numbers], info[msgcell]);  
+                        sheet.getRange(spreadsheetRow, stscell).setValue('Message Sent');
+                        sheet.getRange(spreadsheetRow, leftmsgcell).setValue('Texted');
+                        sheet.getRange(spreadsheetRow, timecell).setValue(timestamp);
                     } catch(err) {
                       Logger.log(err);
-                      status = "error";
+                      sheet.getRange(spreadsheetRow, stscell).setValue('Message NOT Sent');
                       }
-                    sheet.getRange(spreadsheetRow, stscell).setValue(status); //row, column (this is where to print out status) (0=A?) CHANGE
                   }  
                 else
                   {
@@ -91,6 +98,16 @@ for (var i in cellData) {
     }
 }
 
-function myFunction() {
-  sendAll();
+function clearAll() {
+  var startRow = 4; //**********The row to start on change the 2 if numbers start lower down**********
+  var stscell = 10 //*************What Column is your status of the app printed in(A=1, B=2 and so on)******************
+  var timecell = 8 //*************What Column is your timestamp of the app printed in(A=1, B=2 and so on)*****************
+  var leftmsgcell = 6 //*************What Column is your Left Message of the app printed in(A=1, B=2 and so on)*****************
+
+  var sheet = SpreadsheetApp.getActiveSheet();//script runs on the sheet you have opened
+  var numRows = sheet.getLastRow() - 1; //Figures out the last row
+
+  sheet.getRange(startRow, stscell, numRows).clearContent();
+  sheet.getRange(startRow, timecell, numRows).clearContent();
+  sheet.getRange(startRow, leftmsgcell, numRows).clearContent();
 }
